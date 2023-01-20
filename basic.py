769dc4a6e92468297d2b43ebe930928e9464fb6a
PRANAV REDDY SANIKOMMU <772003pranav>
2023-01-19 20:00:31 -0800

    Added basic.py

---
+++ basic.py
@@ -0,0 +1,10 @@
+p = 1.5
+q = 6
+print ("Datatype of p is:",(type(p)))
+print ("Datatype of q is:",(type(q)))
+# Adding a float and an int and storing the result in a sum variable
+sum = p + q 
+ 
+# printing the result
+print("Required sum is:",sum)
+print("Datatype of p+q is:", type(sum))
